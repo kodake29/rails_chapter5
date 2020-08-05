56c1ad7a5f373b49f763bc9f29627acf2bac565a
vagrant <vagrant@localhost.localdomain>
2020-08-05 04:46:20 +0000

    [Finish] 3-8

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -36,6 +36,6 @@ end
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9490f646ccee4f7fb8cdb7f5948b6fb651bf07c6cd62c840e612b9565962939664fb61cf55c8fb94c458db57c2a4e3620a9c7c282f6c52be0e15861f00c8490d'
